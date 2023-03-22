9656ff3a8a068f90cfa1cd460143bceb57811d66
Steven Pak <steven.pak10@gmail.com>
2023-03-22 16:06:40 -0400

    feat: change the login screen

--- mol2db/src/pages/index.tsx
+++ mol2db/src/pages/index.tsx
@@ -1,17 +1,25 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Big_Shoulders_Display, Inter } from 'next/font/google'
 import SubHome from './SubHome'
 import Login from './Login'
+import {useState} from 'react'
+
 // import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
+
+
 
 export default function Home () {
 
   var i =1
   if (i==0){
-    return(<Login/>)
+    return(  
+      // <body className='h-full'>
+        <Login/>  
+      // </body>
+      )
   }else{
     return(<SubHome/>)
   }
